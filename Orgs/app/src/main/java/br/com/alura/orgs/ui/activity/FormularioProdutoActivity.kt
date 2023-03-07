777152a92c9c7f7303525395d53fcf55742a84ae
Luiz Souza <luixsouza12@outlook.com>
2023-03-07 17:02:52 -0300

    criando nova activity para formulario de produtos

---
+++ Orgs/app/src/main/java/br/com/alura/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -0,0 +1,11 @@
+package br.com.alura.orgs.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.alura.orgs.R
+
+class FormularioProdutoActivity : AppCompatActivity(
+    R.layout.activity_formulario_produto
+) {
+
+}
